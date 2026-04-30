f47b7d25b15e8a2482441aa51f09b7e13434643f
Student ApnaCollege <arnavfating09@gmail.com>
2026-05-01 03:00:59 +0530

    Fix ALLOWED_HOSTS for Render deployment

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['arnav-coreyms-django-blog.onrender.com']
 
 
 # Application definition
